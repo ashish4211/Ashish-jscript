87e6607144eaf48c7b10e2dacb80bc244d78d1ad
ashish4211 <128994559+ashish4211@users.noreply.github.com>
2024-08-26 08:32:38 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,36 @@
+"use strict" // treat all jS code as newer version of java script
+/*
+alert("hello") // Gives a error because it is running in nodejs compiler
+not in brower 
+*/
+
+// CODE READABILITY MUST BE GOOD
+console.log(33+33)
+console.log("ashish");
+
+// mdn for reading the documantation 
+// and on ecma tc39.es
+
+let name = "ashish"
+let age = 20
+let isloggedIn = false;
+let state
+
+// Number => 2 to power 53
+// bigint
+// string => "" prefer double coat
+// boolean => true/false
+// null => standalone value
+// undefined => value is not defined for the given variable declared
+// symbol => for finding the uniqueness of key
+
+// object
+// Type of NUll is a object
+
+console.log(typeof null)
+// type of undefined is undefined
+console.log(typeof undefined)
+
+
+
+
