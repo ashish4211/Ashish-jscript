5ff60540d79147eeac25fb0c66a5de59c71da460
ashish4211 <128994559+ashish4211@users.noreply.github.com>
2024-08-26 08:16:07 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accoundId = 434343
+let accountemail = "ashish@gmail.com"
+var accountPassword = "4342"
+
+accountCity = "biharsharif"
+
+// accountId = 1 // NOt allowed
+
+accountemail = "ashishkr@gmail.com"
+accountPassword = "555875"
+accountCity = "patna"
+let acccountstate
+
+console.log(accountCity)
+
+/*
+Prefer not to use var
+becuase of issue in block issue and functional Scope
+*/
+
+console.table([accoundId,accountemail,accountCity,accountPassword,acccountstate])
