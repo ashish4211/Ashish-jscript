7f94921e2a741168e6cca3b3446814314c654372
ashish4211 <128994559+ashish4211@users.noreply.github.com>
2024-08-26 10:56:55 +0000

    conversionOpearation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,5 +29,25 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNo = String(someNumber)
-console.log(typeof stringNo)
-console.log(stringNo)
+// console.log(typeof stringNo)
+// console.log(stringNo)
+
+
+// ****************************** Operation ********************************
+
+let str1 = "ashsih"
+let str2 = "kumar"
+let str3 = str1+str2;
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+console.log((3+4)/ 5 % 3)
+console.log(+true);
+console.log(+"")
+
+// Homework
+// READ DOCUMENTATION ABOUT PREFIX AND POSTFIX JS MDN
